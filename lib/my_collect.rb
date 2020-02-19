534d9bd9a7ce2662c53bbccc003b5a0b3b002e81
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-19 09:55:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
 require pry
 
-def my_collect
+def my_collect(languages)
+  i = 0
+  collection = []
+  while i < array.length
+   name_collection.push yield(array[i])
+   i += 1
+end
+name_collection
+end
+
+  end
